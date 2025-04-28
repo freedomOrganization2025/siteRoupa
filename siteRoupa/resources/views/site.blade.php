7f995055f2bfa36e87ca1f8db56d9cd63fafc576
Gabriel Vinicius <gabrielvini0307@hotmail.com>
2025-04-28 10:22:51 -0300

    Primeiro Commit

---
+++ siteRoupa/resources/views/site.blade.php
@@ -0,0 +1,16 @@
+<div>
+    <!-- Walk as if you are kissing the Earth with your feet. - Thich Nhat Hanh -->
+</div>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    
+</body>
+</html>
